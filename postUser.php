4dfb0d724a9abdd9fefef7731b0a452ec9977086
Jessica <synowski.jessica@hotmail.fr>
2024-05-30 12:01:32 +0200

    foreach hastag

--- postUser.php
+++ postUser.php
@@ -1,34 +1,24 @@
 <article>
-                        <h3>
-                            <time><?php 
-                            $time = strtotime($post['created']);
-                            $newformat = date('d/m/Y à H:i',$time);
-                            echo $newformat ?></time>
-                            
-                        </h3>
-                        <address>par <?php echo $post['author_name']?></address>
-                        <div>
-                            <p><?php echo $post['content']?></p>
-                        </div>
-                        <footer>
-                            <small>♥ <?php echo $post['like_number']?> </small>
-                            <a href="">#<?php 
-                                $tagHashtag=$post['taglist'];
-                                //transforme en tableau la chaine de caractères des tags, idem fct split en js
-                                $tagHastagList=explode(",",$tagHashtag);
-                                echo $tagHastagList[0];
-                            ?></a><?php 
-                                if(isset($tagHastagList[1]))
-                                {
-                                    ?><a href="">, #<?php 
-                                    echo $tagHastagList[1];
-                                }
-                            ?></a><?php 
-                            if(isset($tagHastagList[2]))
-                            {
-                                ?><a href="">, #<?php 
-                                echo $tagHastagList[2];
-                            }
-                        ?></a>
-                        </footer>
-                    </article>
+    <h3>
+        <time><?php 
+            $time = strtotime($post['created']);
+            $newformat = date('d/m/Y à H:i',$time);
+            echo $newformat ?></time>
+    </h3>
+    <a href="wall.php?user_id=<?php echo $post['user_id']?>">
+    <address> par <?php echo $post['author_name']?></address></a>
+    <div>
+        <p><?php echo $post['content']?></p>
+    </div>
+    <footer>
+        <small>♥ <?php echo $post['like_number']?> </small>
+            <?php
+            //transforme en tableau la chaine de caractères des tags, idem fct split en js
+            $tagHastagList=explode(",",$post['taglist']);
+            foreach ($tagHastagList as &$tag){
+            ?>
+            <a href="">#<?php echo $tag;?></a>
+            <?php
+            }?>
+    </footer>
+</article>
