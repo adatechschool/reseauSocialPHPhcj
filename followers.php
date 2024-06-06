66e807a9c7c1a10570d4c78d73c3bc7c04af9f4f
Jessica <synowski.jessica@hotmail.fr>
2024-06-06 08:22:24 +0200

    maj followers

--- followers.php
+++ followers.php
@@ -38,12 +38,20 @@
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 // Etape 4: à vous de jouer
                 //@todo: faire la boucle while de parcours des abonnés et mettre les bonnes valeurs ci dessous 
-                ?>
-                <article>
-                    <img src="user.jpg" alt="blason"/>
-                    <h3>Béatrice</h3>
-                    <p>id:321</p>
+                while ($post = $lesInformations->fetch_assoc())
+                {
+                    ?>
+                    <article>
+                  
+                    <img src="img/user<?php echo $post['id'];?>" alt="blason"/>
+                    <h3><?php echo $post['alias']?></h3>
+                    <p>id:<?php echo $post['id']?></p>                    
                 </article>
+                   
+                   
+              <?php  } ?>
+
+              
             </main>
         </div>
     </body>
